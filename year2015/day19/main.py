990a980e78e2c417fda9c8646293eb870270a391
Lucas Moeskops <lucasmoeskops@gmail.com>
2019-07-30 14:15:52 +0200

    Solution 19!

--- year2015/day19/main.py
+++ year2015/day19/main.py
@@ -1,5 +1,6 @@
-from collections import defaultdict, deque, Counter
-lines = open('input.txt', 'r').read().strip().split('\n')
+from collections import defaultdict
+from sys import argv
+lines = open('input.txt' if len(argv) < 2 else argv[1], 'r').read().strip().split('\n')
 target = lines.pop()
 lines.pop()
 replacements = defaultdict(list)
@@ -14,53 +15,52 @@ for from_ in replacements:
                 new_molecules.add(target[:i] + to_ + target[i+len(from_):])
 print('There can be made {} different molecules after one iteration.'.format(
     len(new_molecules)))
-rev_replacements = []
+# Let's find results that have multiple options
+results = set()
+duplicates = set()
 for from_, tos in replacements.items():
     for to in tos:
-        rev_replacements.append((to, from_))
-uniqueness = Counter()
-for to, from_ in rev_replacements:
-    uniqueness.update(to)
-rev_replacements.sort(key=lambda x: (-min(uniqueness[c] for c in x[0]), x[0]),
-                      reverse=True)
-def find_origin(molecule):
-    def recurse(steps, premolecule):
-        print(premolecule)
-        #if premolecule in known:  # and steps > known[premolecule]:
-        #    return
-        #known[premolecule] = steps
-        if premolecule == 'e':
-            return steps
-        for to, from_ in rev_replacements:
-            found = False
-            while to in premolecule:
-                steps += 1
-                premolecule = premolecule.replace(to, from_, 1)
-                found = True
-            if found:
-                stack.append((
-                    steps,
-                    premolecule))
-                    #premolecule[:i] + from_ + premolecule[i+len(to_):]))
-                break
-    stack = [(0, molecule)]
-    known = set()
-    results = []
-    p = 1000
-    while stack:
-        p -= 1
-        if p == 0:
-            print(len(stack), stack[-1][0], len(stack[-1][1]))
-            p = 1000
-#        best_option = min(stack, key=lambda x: len(x[1]))
-#        if best_option[1] in known:
-#            stack.remove(best_option)
-#            continue
-#        else:
-#            known.add(best_option[1])
-        result = recurse(*stack.pop())
-        if isinstance(result, int):
-            results.append(result)
-    return min(results)
-print('The fastest way to make the molecules takes {} steps.'.format(
-    find_origin(target)))
+        for result in results:
+            if to in result:
+                duplicates.add(to)
+            if result in to:
+                duplicates.add(result)
+        results.add(to)
+if duplicates:
+    print('We assumed there were no duplicates. Sorry')
+    quit()
+# So, no duplicates. Now we can make replacements, but the order might
+# matter. To make order not matter, let's start at the end (or the
+# beginning would be another option). Also not every part might be
+# replaceable immediately, so we add a skip factor if we can't find a
+# replacement.
+# First we need a backwards lookup for results.
+rev_replacements = {}
+for from_, tos in replacements.items():
+    for to in tos:
+        # We already know the replacement is unique
+        rev_replacements[to] = from_
+# Now we can look for matching replacements
+num_replacements = 0
+query_size = 1
+skip_chars = 0
+while query_size <= len(target) and skip_chars < len(target):
+    while query_size + skip_chars <= len(target):
+        slice_end = -skip_chars if skip_chars else len(target)
+        snippet = target[-query_size - skip_chars:slice_end]
+        if snippet in rev_replacements:
+            before = len(target)
+            target = target[:-query_size - skip_chars] + rev_replacements[snippet] + target[slice_end:]
+            num_replacements += 1
+            query_size = 1
+            skip_chars = 0
+        else:
+            query_size += 1
+    skip_chars += 1
+    query_size = 1
+# The target should now be the starting value, 'e'
+if target != 'e':
+    print(f'Something went wrong with the replacements. We got \'{target}\''
+          f' instead of \'e\'')
+    quit()
+print(f'The fewest number of steps to arrive at \'e\' is {num_replacements}.')
